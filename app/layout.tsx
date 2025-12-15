13f51c197c2f20017dc4c51c0dbf213475e05024
dailyeudor49-netizen <dailyeudor49@gmail.com>
2025-12-15 17:02:35 +0100

    Add font weights 800 and 900 for bold/black text

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import FacebookPixel from "./components/FacebookPixel";
 
 const poppins = Poppins({
   subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
+  weight: ["400", "500", "600", "700", "800", "900"],
   variable: "--font-poppins",
 });
 
